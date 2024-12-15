ac077de993f4655ee46ff416676ba5f732f34a6e
mmmmmmmmmmelon <chocolategakirai@gmail.com>
2024-12-15 14:16:52 +0900

    変更をコミット

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,3 +1,11 @@
 const today = new Date();
 
-console.log( today )
+// 年、月、日を取得
+const year = today.getFullYear();
+const month = String(today.getMonth() + 1).padStart(2, '0'); 
+const day = String(today.getDate()).padStart(2, '0');        
+
+// 希望の形式で日付を表示
+const formattedDate = `${year}年${month}月${day}日`;
+
+console.log(formattedDate);
