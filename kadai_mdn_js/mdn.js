793460705f21e41185c9bc9cc0ad70cdc2631d5e
mmmmmmmmmmelon <chocolategakirai@gmail.com>
2024-12-15 14:26:54 +0900

    変更をコミット2

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -2,8 +2,8 @@ const today = new Date();
 
 // 年、月、日を取得
 const year = today.getFullYear();
-const month = String(today.getMonth() + 1).padStart(2, '0'); 
-const day = String(today.getDate()).padStart(2, '0');        
+const month = String(today.getMonth() + 1)
+const day = String(today.getDate())       
 
 // 希望の形式で日付を表示
 const formattedDate = `${year}年${month}月${day}日`;
